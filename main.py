487835ae1cf119e5ce02ea4398c0b20489a89444
Olexiy Korshunov <korshunov.olexiy@gmail.com>
2021-06-03 13:03:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import ctypes, os
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -6,6 +7,23 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
+
+# класс констант для окна сообщений функции ShowMessage
+class _Const(object):
+    ICON_EXLAIM = 0x30  # желтый треугольник
+    ICON_INFO = 0x40    # синий восклицательный знак
+    ICON_STOP = 0x10    # красный крестик
+    MB_OK = 0
+
+CONST = _Const()
+
+def ShowMessage(title, text, btn_icon):
+    # if system is Windows
+    if os.name == 'nt':
+        return ctypes.windll.user32.MessageBoxW(0, text, title, btn_icon)
+    else:
+        print(text)
+
 
 options = Options()
 options.add_argument("--headless")
@@ -30,6 +48,8 @@ try:
     date_exired = datetime.strptime(driver.find_element_by_xpath("/html/body/div[3]/div[5]/div/table/tbody/tr[2]/td[2]/p").text, "%d/%m/%Y")
     date_now = datetime.now()
     date_compare = (date_exired - date_now).days
-    print( f"Интернет отключится через {date_compare} дня(ней). Сумма на счету {sum_count}" if date_compare >= 0 else "Интернет уже отключен. Пополните ваш счет" )
+    ShowMessage('Завершение работы', f"Интернет отключится через {date_compare} дня(ней).\nСумма на счету {sum_count}" if date_compare >= 0 else "Интернет уже отключен. Пополните ваш счет", CONST.MB_OK | CONST.ICON_INFO)
 except TimeoutException:
-    print("Не смог зайти в личный кабинет.")
+    ShowMessage('Произошла ошибка', "Не смог зайти в личный кабинет.")
+
+driver.quit()
