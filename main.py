75627ab073e73db1c064764511d65b16296544ba
Olexiy Korshunov <korshunov.olexiy@gmail.com>
2021-06-28 21:16:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,34 @@
-import ctypes, os
 from datetime import datetime
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import mechanize
 
-# класс констант для окна сообщений функции ShowMessage
-class _Const(object):
-    ICON_EXLAIM = 0x30  # желтый треугольник
-    ICON_INFO = 0x40    # синий восклицательный знак
-    ICON_STOP = 0x10    # красный крестик
-    MB_OK = 0
+url = 'https://stat.sitv.com.ua/personal_log_in'
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_equiv(False)
+br.set_handle_refresh(False)
+br.addheaders = [('User-agent', 'Mozilla/5.0')]
+br.open(url, timeout=10.0)
 
-CONST = _Const()
-
-def ShowMessage(title, text, btn_icon):
-    # if system is Windows
-    if os.name == 'nt':
-        return ctypes.windll.user32.MessageBoxW(0, text, title, btn_icon)
-    else:
-        print(text)
-
-
-options = Options()
-options.add_argument("--headless")
-# if chromedriver not found - load it
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-# load page of SITV provider
-driver.get('https://stat.sitv.com.ua')
-
-login = driver.find_element_by_name("login")
-pwd = driver.find_element_by_name("password")
-btn = driver.find_element_by_name("commit")
-# insert login for your cabinet
-login.send_keys("")
-# insert password for your cabinet
-pwd.send_keys("")
-btn.click()
-
-delay = 20 # seconds
 try:
-    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[5]/div/table/tbody/tr[2]/td[2]/p')))
-    sum_count = driver.find_element_by_xpath("/html/body/div[3]/div[5]/div/table/tbody/tr[1]/td[2]/p").text
-    date_exired = datetime.strptime(driver.find_element_by_xpath("/html/body/div[3]/div[5]/div/table/tbody/tr[2]/td[2]/p").text, "%d/%m/%Y")
-    date_now = datetime.now()
-    date_compare = (date_exired - date_now).days
-    if date_compare == 1"
-        ShowMessage('Время действовать', f"Интернет отключится через {date_compare} день.\nСумма на счету {sum_count}", CONST.MB_OK | CONST.ICON_INFO)
-    if date_compare <= 0:
-        ShowMessage('Надо шо-то делать', f"Похоже интернет уже отключен.\nСумма на счету {sum_count}", CONST.MB_OK | CONST.ICON_INFO)
-except TimeoutException:
-    ShowMessage('Произошла ошибка', "Не смог зайти в личный кабинет.")
-
-driver.quit()
+    form = br.forms()[1]
+    # put your username here
+    form['login'] = ''
+    # put your pasword here
+    form['password'] = ''
+    html = br.open( form.click(name='commit'), timeout=10.0 ).get_data()
+    br.close()
+    bs = BeautifulSoup(html, 'lxml')
+    info_container = bs.findAll('div', {'class': 'info_container'})[1].findAll('p')
+    sum_count, date_expired = (info.text for info in info_container[1::2])
+    date_compare = (datetime.strptime(date_expired, "%d/%m/%Y") - datetime.now()).days
+    if date_compare == 0:
+        print( 'Быстрее пополняй счет.\nИнтернет сегодня отключится!' )
+    elif date_compare < 0:
+        print( "Интернет уже отключен. Пополните ваш счет!" )
+    elif 0 < date_compare <= 2:
+        print( f"Интернет отключится через {date_compare} дня(ей). Сумма на счету {sum_count}" )
+except:
+    br.close()
+    print( 'Не смог зайти в кабинет пользователя.\nПроверьте логин и пароль.' )
+    
